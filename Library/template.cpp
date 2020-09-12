ce8d2662c3b0874cbe5a0b8fe52571294970ae54
gourango-modak <gourango.modak.2@gmail.com>
2020-09-13 02:10:19 +0600

    template i/0 part updated

--- Library/template.cpp
+++ Library/template.cpp
@@ -1,6 +1,6 @@
 #include<bits/stdc++.h>
 using namespace std;
-#define fo(i,n) for(i=0;i<n;i++)
+#define fo(i,n) for(int i=0;i<n;i++)
 #define ll long long
 #define all(x) x.begin(), x.end()
 #define clr(x) memset(x, 0, sizeof(x))
@@ -13,24 +13,32 @@ typedef vector<vi>		vvi;
 //=======================
 const int MOD = 1000000007;
 const int N = 2e6+13, M = N;
-//=======================
+=======================
 vi g[N];
 int a[N];
 int n, m, k, v;
-//=======================
+=======================
 
 void solve() {
-  
+	
 }
 
 int main() {
+	// #ifndef ONLINE
+	#ifndef ONLINE_JUDGE
+	    // for getting input from input.txt
+	    freopen("input.txt", "r", stdin);
+	    // for writing output to output.txt
+	    freopen("output.txt", "w", stdout);
+	#endif
+
     // ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
 	solve();
-    int t = 1;
-    cin >> t;
-    while(t--) {
-      solve();
-    }
+    // int t = 1;
+    // cin >> t;
+    // while(t--) {
+    //   solve();
+    // }
 
     return 0;
 }
