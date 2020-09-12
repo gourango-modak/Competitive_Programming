8399ab9df3e42b6bb354a4ddc597a8db005c96ec
gourango-modak <gourango.modak.2@gmail.com>
2020-09-13 00:55:18 +0600

    template file added

---
+++ Library/template.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define fo(i,n) for(i=0;i<n;i++)
+#define ll long long
+#define all(x) x.begin(), x.end()
+#define clr(x) memset(x, 0, sizeof(x))
+#define sortall(x) sort(all(x))
+#define PI 3.1415926535897932384626
+typedef pair<int, int>	pii;
+typedef vector<int>		vi;
+typedef vector<pii>		vpii;
+typedef vector<vi>		vvi;
+//=======================
+const int MOD = 1000000007;
+const int N = 2e6+13, M = N;
+//=======================
+vi g[N];
+int a[N];
+int n, m, k, v;
+//=======================
+
+void solve() {
+  
+}
+
+int main() {
+    // ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+	solve();
+    int t = 1;
+    cin >> t;
+    while(t--) {
+      solve();
+    }
+
+    return 0;
+}
